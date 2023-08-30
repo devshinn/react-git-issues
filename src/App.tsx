fe3c484a081d75454f6284e8be384c9295d8c1d6
devshinn <shin.github@gmail.com>
2023-08-31 07:18:29 +0900

    Chore :  홈으로 접근시 '/issues'로 리다이렉트 시키기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ErrorPage from './pages/ErrorPage';
 import IssueDetailPage from './pages/IssueDetailPage';
 import Issues from './pages/IssueListPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to={'/issues'} />} />
         <Route path='/issues' element={<Issues />} />
         <Route path='/issues/:issue_id' element={<IssueDetailPage />} />
         <Route path='/*' element={<ErrorPage />} />
